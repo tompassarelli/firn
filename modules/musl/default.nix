c8ca29517ffe6ef8d74b5a691c712eaf749761ca
tompassarelli <tom.passarelli@protonmail.com>
2026-08-18 05:22:15 +0800

    musl: add musl-gcc toolchain module

---
+++ modules/musl/default.nix
@@ -0,0 +1,8 @@
+{ config, lib, pkgs, ... }:
+
+{
+  options.myConfig.modules.musl.enable = lib.mkEnableOption "musl C compiler toolchain";
+  config = lib.mkIf config.myConfig.modules.musl.enable {
+    environment.systemPackages = with pkgs; [ musl ];
+  };
+}
