b1187ba96a742ccf83680d3b80a21e78879afb80
tompassarelli <tom.passarelli@protonmail.com>
2026-08-18 05:42:42 +0800

    musl: install the compiler development output

--- modules/musl/default.nix
+++ modules/musl/default.nix
@@ -3,6 +3,6 @@
 {
   options.myConfig.modules.musl.enable = lib.mkEnableOption "musl C compiler toolchain";
   config = lib.mkIf config.myConfig.modules.musl.enable {
-    environment.systemPackages = with pkgs; [ musl ];
+    environment.systemPackages = with pkgs; [ musl.dev ];
   };
 }
